be2d2f4650cad3f8de20f1d582ebd57bb520d800
if19b020 <if19b020@technikum-wien.at>
2022-10-11 20:21:18 +0200

    removed unused imports

--- src/main/java/at/fhtw/swen3/services/mapper/TruckMapper.java
+++ src/main/java/at/fhtw/swen3/services/mapper/TruckMapper.java
@@ -1,8 +1,6 @@
 package at.fhtw.swen3.services.mapper;
 
-import at.fhtw.swen3.persistence.Transferwarehouse;
 import at.fhtw.swen3.persistence.Truck;
-import at.fhtw.swen3.services.dto.TransferwarehouseDto;
 import at.fhtw.swen3.services.dto.TruckDto;
 import org.mapstruct.factory.Mappers;
 
