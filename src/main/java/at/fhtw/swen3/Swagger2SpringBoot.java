04fdf8824fb8e91298bd8f510ec0bd1dfa69aacd
Manuel T <manuelthuer72@gmail.com>
2022-10-11 19:11:08 +0200

    test branch

--- src/main/java/at/fhtw/swen3/Swagger2SpringBoot.java
+++ src/main/java/at/fhtw/swen3/Swagger2SpringBoot.java
@@ -29,6 +29,7 @@ public class Swagger2SpringBoot implements CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
         new SpringApplication(Swagger2SpringBoot.class).run(args);
+        System.out.println("Hello World!");
     }
 
     @Configuration
